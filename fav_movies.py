92392c3c9439a8399a3023977b3b3493ea66cb3b
Nathon Reed <paultercy@nathonsdev.com>
2016-10-17 09:23:35 -0700

    fix error on line 27

--- fav_movies.py
+++ fav_movies.py
@@ -3,15 +3,15 @@ import media #import class Movie
 
 #Movies that will be displayed in the html file
 Harry_Potter = media.Movie("Harry Potter and the Goblet of Fire",
-	"Harry Potter's fourth year at Hogwarts as he is chosen by the Goblet of Fire to compete in the Triwizard Tournament.",
+	"Harry Potter's  is chosen by the Goblet of Fire to compete in the Triwizard Tournament.",
 	"http://www.wichitaorpheum.com/wp-content/uploads/2013/05/Harry-Potter-and-the-Goblet-of-Fire-movie-poster.jpg",
 	"https://www.youtube.com/watch?v=WVNENtEJyMM")
 Chappie = media.Movie("Chappie",
-	"The film, set and shot in johannesburg, is about an artificially intelligent law enforcement robot captured",
+	"Is about an artificially intelligent law enforcement robot captured",
 	"http://www.joblo.com/images_arrownews/chappie-international-poster.jpg",
 	"https://www.youtube.com/watch?v=l6bmTNadhJE")
 Star_Trek_Beyond = media.Movie("Star Trek Beyond",
-	"Three years into its five year mission, the USS Enterprise arrives at Starbase Yorktown, a massive space station",
+	"The USS Enterprise arrives at Starbase Yorktown, a massive space station",
 	"http://getthechance.wales/wp-content/uploads/2016/08/star-trek-beyond-poster-2.jpg",
 	"https://www.youtube.com/watch?v=XRVD32rnzOw")
 Air_Force_One = media.Movie("Air Force One", 
@@ -23,9 +23,9 @@ Jurassic_World = media.Movie("Jurassic World",
 	"http://www.impawards.com/2015/posters/jurassic_world_ver3.jpg",
 	"https://www.youtube.com/watch?v=aJJrkyHas78")
 Legend = media.Movie("I Am Legend",
-	"A genetically re-engineered measles virus, originally created as a cure for cancer, turns into a lethal",
+	"A genetically re-engineered measles virus, turns into a lethal",
+	"http://moviefiles.alphacoders.com/558/poster-558.jpg",
 	"https://www.youtube.com/watch?v=ewpYq9rgg3w")
-
 movies = [Harry_Potter, Chappie, Air_Force_One, Jurassic_World, Legend, Star_Trek_Beyond]
 #takes array of movies so fresh_tomatoes class can render the html file
 fresh_tomatoes.open_movies_page(movies)
